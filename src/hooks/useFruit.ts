f5047d8e3d19a9a3837a7262b2ffef660ddea3aa
Ted Leahy <tleahy@uplandsoftware.com>
2023-05-27 13:33:07 +0100

    Fix bug where fruit wasn't appearing after restarting a game

--- src/hooks/useFruit.ts
+++ src/hooks/useFruit.ts
@@ -6,6 +6,37 @@ import { CanvasLocation } from "../types/general";
 
 const { fruitColour, gridSquareSize } = settings
 
+export default function useFruit(
+  ctx: CanvasRenderingContext2D | null | undefined,
+  snake: Snake,
+  setSnake: React.Dispatch<React.SetStateAction<Snake>>,
+) {
+  const [fruitLocation, setFruitLocation] = useState<CanvasLocation>({ x: 0, y: 0})
+  const snakeHasHitFruit = snake.head.x === fruitLocation.x &&
+    snake.head.y === fruitLocation.y
+
+  function drawRandomFruit(): void {
+    if (ctx) {
+      const newLocation = getRandomBoardSquare(ctx.canvas)
+      setFruitLocation(newLocation)
+      drawSquare(ctx, newLocation.x, newLocation.y, fruitColour)
+    }
+  }
+
+  // When snake starts moving
+  useEffect(() => {
+    if (!snake.stoppedMoving) drawRandomFruit()
+  }, [snake.stoppedMoving])
+
+  // When snake hits fruit
+  useEffect(() => {
+    if (ctx && snakeHasHitFruit) {
+      setSnake({ ...snake, maxTailLength: snake.maxTailLength + 1 })
+      drawRandomFruit()
+    }
+  }, [ctx, snakeHasHitFruit])
+}
+
 function randomBetween(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
@@ -17,36 +48,4 @@ function getRandomBoardSquare(canvas: HTMLCanvasElement | undefined): CanvasLoca
     x: Math.floor(randomBetween(0, canvas.width) / gridSquareSize),
     y: Math.floor(randomBetween(0, canvas.height) / gridSquareSize),
   }
-}
-
-export default function useFruit(
-  ctx: CanvasRenderingContext2D | null | undefined,
-  snake: Snake,
-  setSnake: React.Dispatch<React.SetStateAction<Snake>>,
-) {
-  const [fruitLocation, setFruitLocation] = useState<CanvasLocation>({ x: 0, y: 0})
-  
-  const snakeHasHitFruit = snake.head.x === fruitLocation.x
-    && snake.head.y === fruitLocation.y
-
-  const drawRandomFruit = () => {
-    if (ctx) {
-      const newLocation = getRandomBoardSquare(ctx.canvas)
-      setFruitLocation(newLocation)
-      drawSquare(ctx, newLocation.x, newLocation.y, fruitColour)
-    }
-  }
-
-  // When canvas first loads
-  useEffect(() => {
-    drawRandomFruit()
-  }, [ctx])
-
-  // When snake hits fruit
-  useEffect(() => {
-    if (ctx && snakeHasHitFruit) {
-      setSnake({ ...snake, maxTailLength: snake.maxTailLength + 1 })
-      drawRandomFruit()
-    }
-  }, [ctx, snakeHasHitFruit])
 }
