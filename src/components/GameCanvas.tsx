196b0b42c8e34c7b463eb9c761f0c6ad46f2f6d9
Ted Leahy <tleahy@uplandsoftware.com>
2023-05-27 17:34:54 +0100

    Remove unused imports

--- src/components/GameCanvas.tsx
+++ src/components/GameCanvas.tsx
@@ -1,10 +1,6 @@
-import { useRef } from "react";
-import { settings } from "../lib/gameState";
 import useFruit from "../hooks/useFruit";
 import useSnake from "../hooks/useSnake";
 import useCanvas from "../hooks/useCanvas";
-
-const { gridSquareSize, snakeColour, fruitColour, gameOverColour, snakeSpeed} = settings
 
 export default function GameCanvas() {
   const [canvasRef, ctx] = useCanvas()
